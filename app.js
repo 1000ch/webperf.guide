10e2d120db132467bfec03117b32a22546c7d95a
1000ch <shogo.sensui@gmail.com>
2017-11-19 00:24:14 +0900

    Base impl

---
+++ app.js
@@ -0,0 +1,5 @@
+import TwitterButton from './twitter-button.js';
+import FacebookButton from './facebook-button.js';
+
+customElements.define('twitter-button', TwitterButton);
+customElements.define('facebook-button', FacebookButton);
