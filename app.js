79702e7cd57df86e26138f9d8b4ad52b6a0b7f3c
1000ch <shogo.sensui@gmail.com>
2017-11-19 15:20:14 +0900

    Update

--- app.js
+++
@@ -1,5 +0,0 @@
-import TwitterButton from './twitter-button.js';
-import FacebookButton from './facebook-button.js';
-
-customElements.define('twitter-button', TwitterButton);
-customElements.define('facebook-button', FacebookButton);
