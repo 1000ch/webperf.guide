a0c6145059701d6877abb556293ba27221cf3682
1000ch <shogo.sensui@gmail.com>
2017-11-25 22:46:19 +0900

    Handle install and activate events

--- public/service-worker.js
+++ public/service-worker.js
@@ -20,6 +20,18 @@ const CACHE_FILES = [
   'facebook-button.js'
 ];
 
+self.addEventListener('install', event => {
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', event => {
+  const deletion = caches.keys()
+    .then(keys => keys.filter(key => key !== CACHE_KEY))
+    .then(keys => Promise.all(keys.map(key => caches.delete(key))));
+
+  event.waitUntil(deletion.then(() => self.clients.claim()));
+});
+
 self.addEventListener('fetch', event => {
   if (!CACHE_FILES.some(file => event.request.url.includes(file))) {
     return;
