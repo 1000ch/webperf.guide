1f16527a26f491b0cf842e0fcd23cb83e5db7488
1000ch <shogosensui@gmail.com>
2020-07-19 23:55:26 +0900

    Remove terser

--- bin/minify.js
+++ bin/minify.js
@@ -1,17 +1,10 @@
 const fs = require('fs');
 const globby = require('globby');
 const csso = require('csso');
-const terser = require('terser');
 
 const cssFiles = globby.sync(['public/css/*.css']);
-const jsFiles = globby.sync(['public/js/*.js']);
 
 for (const cssFile of cssFiles) {
   const css = fs.readFileSync(cssFile).toString();
   fs.writeFileSync(cssFile, csso.minify(css).css);
-}
-
-for (const jsFile of jsFiles) {
-  const js = fs.readFileSync(jsFile).toString();
-  fs.writeFileSync(jsFile, terser.minify(js).code);
 }
